7e54d14fb4eedc502e9d138147b902f19d693486
Katerinne Fernanda Peralta <peraltakaterinnefernanda@gmail.com>
2023-09-29 19:11:07 -0300

    add a new activity Mis Alarmas 1

---
+++ app/src/main/java/com/dpozzo68/analizarapp/MisAlarmas1.java
@@ -0,0 +1,13 @@
+package com.dpozzo68.analizarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MisAlarmas1 extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_misalarmas1);
+    }
+}
