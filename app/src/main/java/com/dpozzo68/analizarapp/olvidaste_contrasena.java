7975e1a0310e93e94b6a451c8edb9a20fb7bc332
NoeVelaz <velazquezmoemi74@gmail.com>
2023-10-01 18:29:11 -0300

    Renombrar Activity

---
+++ app/src/main/java/com/dpozzo68/analizarapp/olvidaste_contrasena.java
@@ -0,0 +1,14 @@
+package com.dpozzo68.analizarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class olvidaste_contrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_olvidaste_contrasena);
+    }
+}
