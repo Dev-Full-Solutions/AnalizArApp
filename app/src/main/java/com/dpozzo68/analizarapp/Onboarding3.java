90d9f99734a974aabe12a11022ef48dde73d784c
DiegolPl <diegopdl1992@gmail.com>
2023-10-02 02:26:50 -0300

    Cambio diseno de onboarding 3 y le agrego navegabilidad

--- app/src/main/java/com/dpozzo68/analizarapp/Onboarding3.java
+++ app/src/main/java/com/dpozzo68/analizarapp/Onboarding3.java
@@ -2,7 +2,9 @@ package com.dpozzo68.analizarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Onboarding3 extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Onboarding3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding3);
+    }
+
+    public void atras (View view){
+        Intent intent = new Intent(this, Onboarding2.class);
+        startActivity(intent);
+    }
+
+    public void seguir (View view){
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
     }
 }
