693ed1152ad7c032f56df807ec329d3f9474ee6f
DiegolPl <diegopdl1992@gmail.com>
2023-10-02 01:38:41 -0300

    Agrego activity onboarding 1 y su navegabilidad

---
+++ app/src/main/java/com/dpozzo68/analizarapp/Onboarding1.java
@@ -0,0 +1,26 @@
+package com.dpozzo68.analizarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Onboarding1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding1);
+    }
+
+    public void atras (View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void seguir (View view){
+        Intent intent = new Intent(this, Onboarding2.class);
+        startActivity(intent);
+    }
+}
