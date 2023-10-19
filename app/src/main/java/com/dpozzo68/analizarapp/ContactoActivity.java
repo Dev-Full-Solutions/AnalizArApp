3e8824073a4416e4ea6d26a3ca3769a11ed8083a
Carlos Daniel Pozzo <dpozzo68@gmail.com>
2023-10-19 01:47:29 -0300

    Actualizaciones sobre la funcionalidad del enlace en la activity de Contacto y de formatos varios

--- app/src/main/java/com/dpozzo68/analizarapp/ContactoActivity.java
+++ app/src/main/java/com/dpozzo68/analizarapp/ContactoActivity.java
@@ -1,36 +1,63 @@
 package com.dpozzo68.analizarapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ContactoActivity extends AppCompatActivity {
+    /*
+    Bloque comentado el 18-10 Inicio
+    Código a reutilizar en la funcionalidad del botón SOPORTE
     Button btnInicio;
-    // Declaro la variable btnInicio indicando que es de tipo Button
+    Bloque comentado el 18-10 Fin
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
-        TextView textView = findViewById(R.id.textView);
-        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        // Código añadido el 18-10 Inicio
+        TextView textView = findViewById(R.id.textView10);
+        String linkText = getResources().getString(R.string.link);
+        SpannableString spannableString = new SpannableString(linkText);
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                String url = "https://github.com/Dev-Full-Solutions";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        };
 
-//        btnInicio = findViewById(R.id.button4);
-//        btnInicio.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(ContactoActivity.this, MisConsumos.class);
-//                startActivity(intent);
-//            }
-//        });
+        //Código añadido el 18-10 Fin
+
+        /*
+        Bloque comentado el 18-10 Inicio
+         Código a reutilizar para la navegación entre el botón SOPORTE y la activity con el formulario con la consulta del usuario (Funcionalidad)
+         TextView textView = findViewById(R.id.textView);
+         textView.setMovementMethod(LinkMovementMethod.getInstance());
+         btnInicio = findViewById(R.id.button4);
+         btnInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactoActivity.this, MisConsumos.class);
+                startActivity(intent);
+            }
+         });
+        Bloque comentado el 18-10 Fin
+        */
     }
-    // Código para volver a la activity Main.
+
     public void irConsumos(View view) {
         ImageView imagen = findViewById(R.id.imagen_home);
         imagen.setClickable(true);
